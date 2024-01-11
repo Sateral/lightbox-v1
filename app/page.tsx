46d0a165b7af0008c8b13e672b7fd207de4825d4
Satty <kopylenkod@gmail.com>
2024-01-11 01:06:58 -0500

    compressed images, fixed grid

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ import { useLightboxStore } from "@/hooks/useLightBoxModal";
 
 const Home = () => {
   const images = [
-    "/alley-way.jpg",
-    "/desert.jpg",
-    "/diver.jpg",
-    "/fox.jpg",
-    "/house.jpg",
-    "/lion.jpg",
-    "/mountain-climb.jpg",
-    "/plant.jpg",
+    "/alley-way.webp",
+    "/desert.webp",
+    "/diver.webp",
+    "/fox.webp",
+    "/house.webp",
+    "/lion.webp",
+    "/mountain-climb.webp",
+    "/plant.webp",
   ];
 
   const Lightbox = useLightboxStore();
@@ -24,20 +24,21 @@ const Home = () => {
 
   return (
     <>
-      <main className="bg-slate-500">
+      <main className="bg-[#111]">
         <p className="p-2 text-2xl font-sans font-medium text-white">
           Creating a super reusable Lightbox Modal!
         </p>
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 m-12">
           {images.map((image, index) => (
-            <div key={index}>
-              <Image
+            <div
+              key={index}
+              className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 border-white border-2"
+            >
+              <img
                 src={image}
                 alt="test"
-                width={250}
-                height={250}
                 onClick={() => onLightbox(index)}
-                className="h-auto"
+                className="object-cover w-full h-full"
               />
             </div>
           ))}
